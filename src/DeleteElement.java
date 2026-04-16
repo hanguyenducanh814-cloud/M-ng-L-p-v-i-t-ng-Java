14dc79edd137e4f2af6ae1278d80ba3477249260
Nguyen Duc Anh <ducanh @gmail.com>
2026-04-16 16:04:56 +0700

    Hoàn thành bài 7

---
+++ src/DeleteElement.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class DeleteElement {
+    public static void main(String[] args) {
+        int[] arr = {10, 4, 6, 7, 8, 6, 0, 0, 0, 0};
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Nhập phần tử cần xoá: ");
+        int x = scanner.nextInt();
+        int index_del = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == x) {
+                index_del = i;
+                break;
+            }
+        }
+
+        if (index_del != -1) {
+            System.out.println("Đã tìm thấy " + x + " tại vị trí " + index_del);
+            for (int i = index_del; i < arr.length - 1; i++) {
+                arr[i] = arr[i + 1];
+            }
+            arr[arr.length - 1] = 0;
+            System.out.print("Mảng sau khi xoá: ");
+            for (int element : arr) {
+                System.out.print(element + " ");
+            }
+        } else {
+            System.out.println("Phần tử " + x + " không tồn tại trong mảng.");
+        }
+    }
+}
