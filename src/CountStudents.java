fe3ed18330cac886e6ffe7b8437a5ea635125dab
Nguyen Duc Anh <ducanh @gmail.com>
2026-04-17 09:30:51 +0700

    Hoàn thành bài 11

---
+++ src/CountStudents.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class CountStudents {
+    public static void main(String[] args){
+        int size;
+        double[] array;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.print("Enter a size:");
+            size = scanner.nextInt();
+            if (size > 30)
+                System.out.println("Size should not exceed 30");
+        } while (size > 30);
+        array = new double[size];
+        int i = 0;
+        while (i < array.length) {
+            System.out.print("Enter a mark for student " + (i + 1) + ": ");
+            array[i] = scanner.nextDouble();
+            i++;
+        }
+        double count = 0;
+        System.out.print("List of mark: ");
+        for (int j = 0; j < array.length; j++) {
+            System.out.print(array[j] + "\t");
+            if (array[j] >= 5 && array[j] <= 10)
+                count++;
+        }
+        System.out.print("\n The number of students passing the exam is " + count);
+    }
+}
