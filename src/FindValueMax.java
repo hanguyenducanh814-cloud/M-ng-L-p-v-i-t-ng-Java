e1602be68140d989646f9e6f190d248d4b26125b
Nguyen Duc Anh <ducanh @gmail.com>
2026-04-17 09:08:39 +0700

    Hoàn thành bài 9

---
+++ src/FindValueMax.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class FindValueMax {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] arr = new int[5];
+        System.out.println("Nhập 5 phần tử cho mảng:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("Phần tử " + i + ": ");
+            arr[i] = scanner.nextInt();
+        }
+        int max = findValueMax(arr);
+        System.out.println("Giá trị lớn nhất trong mảng là: " + max);
+    }
+    public static int findValueMax(int[] array) {
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+}
